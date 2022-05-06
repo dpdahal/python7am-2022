a6f2a8e1f2ff815452ec67548aae349f10bf26f4
dpdahal <dp48dahal@gmail.com>
2022-05-06 08:59:02 +0545

    nested if else

---
+++ conditional/nestedifelsestatements.py
@@ -0,0 +1,65 @@
+# x = 100
+# y = 20
+# z = 300
+#
+# if x > y:
+#     if x > z:
+#         print('x is greater than y and z')
+#     else:
+#         print('z ls larger than x and y')
+#
+# else:
+#     if y > z:
+#         print('y is greater than z')
+#     else:
+#         print('z is greater than y')
+
+
+# username = 'admin'
+# password = '12345'
+#
+# if username == 'admin':
+#     if password == '12345':
+#         print('Login successful')
+#     else:
+#         print('Wrong password')
+#
+# else:
+#     if password == '12345':
+#         if username == 'admin':
+#             print('Login successful')
+#         else:
+#             print('Wrong username')
+#     else:
+#         print('Username & password wrong')
+
+# if username == 'adminn' and password == '12345':
+#     print('You are logged in')
+# else:
+#     print('Username & password is not correct')
+
+
+# even and odd number
+
+# numbers = 11
+#
+# if numbers % 2 == 0:
+#     print('Even number')
+# else:
+#     print('Odd number')
+
+# WAP to check if 3 and 5 are multiples
+
+# numbers = 15
+# if numbers % 3 == 0 and numbers % 5 == 0:
+#     print('3 and 5 are multiples')
+# else:
+#     print('3 and 5 are not multiples')
+#
+
+
+x, y, *z = 10, 30, 50, 60, 70, 80
+
+print(x)
+print(y)
+print(z)
--- introduction.py
+++
@@ -1,2 +0,0 @@
-print("hello python introduction")
-print('second message')
