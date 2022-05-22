0a87da7b9daeab626371ee7bdad345a6ac672336
dpdahal <dp48dahal@gmail.com>
2022-05-22 08:49:25 +0545

    update

---
+++ oop/day2/Mobile.py
@@ -0,0 +1,27 @@
+class Mobile:
+
+    def __init__(self, company_name):
+        print(company_name)
+
+    def brand(self, brand):
+        return f'Brand {brand}'
+
+    def model(self, model):
+        return f'Model {model}'
+
+    def price(self, price):
+        return f'Price {price}'
+
+
+class Mi(Mobile):
+    def __init__(self, company_name, logo):
+        # Mobile.__init__(self, company_name)
+        super().__init__(company_name)
+        print(logo)
+
+    def memory(self, memory):
+        return f'Memory {memory}'
+
+
+obj = Mi('Xiaomi', 'X')
+print(obj.brand('Xiaomi'))
