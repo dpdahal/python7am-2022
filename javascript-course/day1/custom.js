df07dc4497fb84ffb547989829dd82e6f7fd7364
dpdahal <dp48dahal@gmail.com>
2022-06-07 09:11:30 +0545

    js introduction

---
+++ javascript-course/day1/custom.js
@@ -0,0 +1,143 @@
+"use strict";
+
+// alert('test')
+
+// document.write("<h1>Hello JavaScript</h1>");
+
+// console.log('test was success')
+
+// document.getElementById('test').innerText="<b>test</b>";
+// document.getElementById('test').innerHTML="<b>test</b>";
+
+// _ =10;
+// console.log($);
+
+// var,let,const
+
+// var x = 10
+//
+// console.log(x)
+
+
+// for (let x = 0; x < 10; x++) {
+//     console.log(x)
+// }
+// console.log('tst');
+// console.log(x)
+
+// const a =10
+// a+=20
+// console.log(a)
+
+// alert('test');
+
+// debugger
+
+// console.log(console);
+// let a = 10;
+// console.log(typeof (a))
+
+// let a
+// console.log(a)
+// console.log
+
+// let users = ['ram', 'sita', 'gita']
+// console.log(users)
+// console.table(users)
+
+// let users = new Array('ram');
+// console.log(users)
+
+// let users = {
+//     name: 'ram',
+//     address: 'kathmandu',
+//     contact: {
+//         phone: 9876,
+//         mobile: 98766
+//     }
+// }
+//
+// console.log(users.contact.phone)
+//
+// let data = new Set(['ram', 'sita', 'ram'])
+// console.log(data)
+
+// let x = 10
+// let y = 20
+// let z = 30
+//
+// if (x > y && x > z) {
+//     console.log('x')
+//
+// } else if (y > x && y > z) {
+//     console.log('y')
+// } else {
+//     console.log('z')
+// }
+
+//for,while,do while
+//
+// for (let a = 0; a < 10; a++) {
+//     console.log(a);
+// }
+
+// let a =0;
+// while (a<10){
+//     console.log(a)
+//     a++
+// }
+
+// let a = 0;
+// do {
+//     console.log(a)
+//     a++;
+// } while (a < 10);
+
+let data = ['ram', 'sita', 'gita']
+var names = []
+data.map(name => {
+    // names.push(name)
+
+})
+
+// console.log(names)
+
+// data.forEach(function (x){
+//     console.log(x)
+// })
+
+// console.log(data);
+
+// for(let x of data){
+//     console.log(x)
+// }
+
+// for(let x in data){
+//     console.log(data[x])
+// }
+
+// for (let x = 0; x < data.length; x++) {
+//     console.log(data[x])
+// }
+
+
+// setTimeout(function () {
+// alert('test');
+// }, 2000);
+
+// setInterval(function (){
+//     alert('test');
+// },3000);
+
+
+// console.log(new Date())
+
+// function get_times() {
+//     let dd = new Date();
+//     let tm = dd.toLocaleTimeString();
+//     document.getElementById('test').innerHTML = tm;
+// }
+//
+// setInterval(function () {
+//     get_times();
+// }, 1000);
